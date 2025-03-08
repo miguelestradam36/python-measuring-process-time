19bc62792dc3a2be5455e34ea5ff723b1dcbc77c
miguelestradam36 <miguelestradam36@gmail.com>
2025-03-07 23:40:32 -0600

    Uploading execution time project

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -1,4 +1,11 @@
 import pytest
-def test_denpendency():
+@pytest.fixture
+def example_module():
+    return "requests"
+def test_denpendency(example_module):
+    """
+    This test is used only for measuring the execution time
+    """
     import sys
+    assert __import__(str(example_module))
     assert sys.version_info[0] <= 3
